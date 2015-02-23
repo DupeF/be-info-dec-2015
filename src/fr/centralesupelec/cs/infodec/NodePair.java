93c80e3c43c87e745b268e07a6d23b33f78668d9
DupeF <dupe.francis@gmail.com>
2015-02-23 10:34:48 +0100

    Implem des deux méthode restantes de NodePair

--- src/fr/centralesupelec/cs/infodec/NodePair.java
+++ src/fr/centralesupelec/cs/infodec/NodePair.java
@@ -1,5 +1,8 @@
 package fr.centralesupelec.cs.infodec;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.neo4j.graphdb.Node;
 
 import weka.core.Attribute;
@@ -87,13 +90,88 @@ public class NodePair {
 	}
 
 	private double nicknamesSimilarity() {
-		// TODO Auto-generated method stub
-		return 0;
+		String[] nicknames1 = ((String) first.getProperty("nicknames")).split(" ");
+		String[] nicknames2 = ((String) second.getProperty("nicknames")).split(" ");
+		double sim = 0;
+		for(String nickname1 : nicknames1) {
+			for(String nickname2 : nicknames2) {
+				sim = Math.max(sim, normalizedLevenshteinDistance(nickname1, nickname2));
+			}
+		}
+		return sim;
 	}
 	
+
+    public static double normalizedLevenshteinDistance(CharSequence s, CharSequence t)
+    {
+        // degenerate cases
+        if (s == null || "".equals(s))
+        {
+            return t == null || "".equals(t) ? 0 : t.length();
+        }
+        else if (t == null || "".equals(t))
+        {
+            return s.length();
+        }
+
+        // create two work vectors of integer distances
+        int[] v0 = new int[t.length() + 1];
+        int[] v1 = new int[t.length() + 1];
+
+        // initialize v0 (the previous row of distances)
+        // this row is A[0][i]: edit distance for an empty s
+        // the distance is just the number of characters to delete from t
+        for (int i = 0; i < v0.length; i++)
+        {
+            v0[i] = i;
+        }
+
+        int sLen = s.length();
+        int tLen = t.length();
+        for (int i = 0; i < sLen; i++)
+        {
+            // calculate v1 (current row distances) from the previous row v0
+
+            // first element of v1 is A[i+1][0]
+            //   edit distance is delete (i+1) chars from s to match empty t
+            v1[0] = i + 1;
+
+            // use formula to fill in the rest of the row
+            for (int j = 0; j < tLen; j++)
+            {
+                int cost = (s.charAt(i) == t.charAt(j)) ? 0 : 1;
+                v1[j + 1] = minimum(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost);
+            }
+
+            // copy v1 (current row) to v0 (previous row) for next iteration
+            System.arraycopy(v1, 0, v0, 0, v0.length);
+        }
+
+        double levenshteinDistance = v1[t.length()];
+        double tailleMax = Math.max(sLen, tLen);
+        double result = (tailleMax > 0) ? 1-(levenshteinDistance/tailleMax) : 0;
+        return result;
+    }
+    
+    private static int minimum(int a, int b,  int c) {
+    	return Math.min(c, Math.min(a, b));
+    }
+	
 	private double realnameSimilarity() {
-		// TODO Auto-generated method stub
-		return 0;
+		// Mesure Jaccard
+		String[] realname1 = ((String) first.getProperty("realname")).split(" ");
+		String[] realname2 = ((String) second.getProperty("realname")).split(" ");
+		Set<String> differentWords = new HashSet<String>();
+		for(String word  : realname1) {
+			differentWords.add(word);
+		}
+		for(String word : realname2) {
+			if(!differentWords.contains(word))
+				differentWords.add(word);
+		}
+		double totalNumber = realname1.length + realname2.length;
+		double motsCommuns = totalNumber - differentWords.size(); 
+		return motsCommuns/totalNumber;
 	}
 	
 	/**
